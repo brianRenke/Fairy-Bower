b09aa6b309f0b4cb53a8df0a9c461273a85b3fd9
Brian Renke <brian@fairy-bower.com>
2019-02-02 20:39:22 -0800

    navbar and sideDrawer edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
 class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  menuButtonClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false});
+  };
+
   render() {
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
+    }
     return (
-      <div className="App">
-        <Navbar />
+      <div className="app">
+        <Navbar sideDrawerClickHandler={this.menuButtonClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
         <div className="main col-xs">
         	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import MenuButton from '../SideDrawer/MenuButton';
 import '../../css/navbar.css';
 
 const Navbar = props => (
-	<header className="navbar">
+	<header>
 		<nav className="navbar_navigation row middle-xs">
-			<div className="navbar_logo"><a href="/">Fairy Bower</a></div>
-			<div className="spacer" />
-			<div className="navbar_navigation-links">
+			<div className="navbar_logo col-xs-6"><a href="/">Fairy Bower</a></div>
+			<div className="navbar_navigation-links col-xs-6">
+				<MenuButton click={props.sideDrawerClickHandler} />
 				<ul>
 					<li><a href="/">About</a></li>
 					<li><a href="/">Work</a></li>
