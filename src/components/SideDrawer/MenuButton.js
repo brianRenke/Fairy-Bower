0179e2c15b6a99ddd4e04bb69679159d03514d7e
Brian Renke <brian@fairy-bower.com>
2019-02-27 15:17:48 -0800

    navbar and sideDrawer edits

--- src/components/SideDrawer/MenuButton.js
+++ src/components/SideDrawer/MenuButton.js
@@ -5,7 +5,7 @@ const MenuButton = props => (
 	<button className="menu-button" onClick={props.click}>
 		<div className="nav-icon">
     		<svg width="18" height="11" viewBox="0 0 18 11">
-      			<path fill="#000" fill-rule="evenodd" d="M0,10 L18,10 L18,11 L0,11 L0,10 Z M0,5 L18,5 L18,6 L0,6 L0,5 Z M0,0 L18,0 L18,1 L0,1 L0,0 Z"></path>
+      			<path fill="" fill-rule="evenodd" d="M0,10 L18,10 L18,11 L0,11 L0,10 Z M0,5 L18,5 L18,6 L0,6 L0,5 Z M0,0 L18,0 L18,1 L0,1 L0,0 Z"></path>
       		</svg>
     	</div>
 	</button>
