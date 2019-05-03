1bb7f5e8758d7f48807eb67754b5463590109cbf
Brian Renke <brian@fairy-bower.com>
2019-05-03 12:08:01 -0700

    add h1, subheader, and button

--- src/App.js
+++ src/App.js
@@ -29,8 +29,10 @@ class App extends Component {
         <Navbar sideDrawerClickHandler={this.menuButtonClickHandler} />
         <SideDrawer show={this.state.sideDrawerOpen} />
         {backdrop}
-        <div className="main col-xs">
-        	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="main col-xs center-xs">
+          <h1>User Friendly Design</h1>
+        	<p class="body-large">We work with people to build simple, beautiful and intelligent product solutions to complex problems.</p>
+          <a href="#" class="primary-button">View Work</a>
         </div>
       </div>
     );
