6ad14d9682933e3154bef754f1c771a5d7ff6967
Brian Renke <brian@fairy-bower.com>
2019-08-12 15:48:55 -0700

    Updates

---
+++ precache-manifest.84ca9757d39e784d8825c3dc5e51e9bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3963fe67916864f0ebc5",
+    "url": "/fairy-bower/static/js/runtime~main.3963fe67.js"
+  },
+  {
+    "revision": "98c2af4671cb471a974e",
+    "url": "/fairy-bower/static/js/main.98c2af46.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/fairy-bower/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "98c2af4671cb471a974e",
+    "url": "/fairy-bower/static/css/main.2bfecf9c.chunk.css"
+  },
+  {
+    "revision": "2b5d326d214fa7f363bd447b96ba3485",
+    "url": "/fairy-bower/index.html"
+  }
+];
--- src/components/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../../css/backdrop.css';
-
-const Backdrop = props => (
-	<div className="backdrop" onClick={props.click} />
-);
-
-export default Backdrop;
