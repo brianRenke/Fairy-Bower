d57e2aacdb2af86f0d155877bfb9c41bf45441ce
Brian Renke <brian@fairy-bower.com>
2019-11-29 23:22:00 -0800

    Updates

---
+++ precache-manifest.557d2629ca8152dd422950daf4435a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f518389721e21675412",
+    "url": "/static/js/main.9f518389.chunk.js"
+  },
+  {
+    "revision": "b67d5974c58ad2763686",
+    "url": "/static/js/1.b67d5974.chunk.js"
+  },
+  {
+    "revision": "9f518389721e21675412",
+    "url": "/static/css/main.df4c0747.chunk.css"
+  },
+  {
+    "revision": "c968cfd342557bf11a9d1a15c64c885b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fairy-bower/precache-manifest.d0827ddaf298f56aa5c3fdd40383d32a.js"
+  "/precache-manifest.557d2629ca8152dd422950daf4435a83.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/fairy-bower/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
