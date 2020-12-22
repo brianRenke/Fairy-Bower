3edae85f7faa74ce625a0db69e73f5d179da372e
Brian Renke <brian@fairy-bower.com>
2020-12-21 21:43:06 -0800

    Updates

--- precache-manifest.8bcbac52b0aa2f27eedc35193136b407.js
+++ precache-manifest.8bcbac52b0aa2f27eedc35193136b407.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/website/static/js/runtime~main.df8eefd1.js"
   },
   {
-    "revision": "70a67d44a1b655f4d3a9",
-    "url": "/website/static/js/main.70a67d44.chunk.js"
+    "revision": "f7cd8b71ff5935143f4d",
+    "url": "/website/static/js/main.f7cd8b71.chunk.js"
   },
   {
     "revision": "d8cc31f64b20df91e67f",
     "url": "/website/static/js/1.d8cc31f6.chunk.js"
   },
   {
-    "revision": "70a67d44a1b655f4d3a9",
-    "url": "/website/static/css/main.2253c51e.chunk.css"
+    "revision": "f7cd8b71ff5935143f4d",
+    "url": "/website/static/css/main.36851d88.chunk.css"
   },
   {
-    "revision": "9e38c2d092986bfff275fa76069e1851",
+    "revision": "965293ba3a93f37fd9d09313e1bd2e9d",
     "url": "/website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.dc0b6db2e760a7d47c2a9b33c8a6b34b.js"
+  "/website/precache-manifest.8bcbac52b0aa2f27eedc35193136b407.js"
 );
 
 workbox.clientsClaim();
