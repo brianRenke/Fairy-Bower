a240d4df8eb77f2804af1cbab785e74343d1336c
Brian Renke <brian.renke@Brians-MacBook-Pro-2.local>
2021-03-21 18:55:02 -0700

    refactored navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import MenuButton from './MenuButton';
-import NavbarIcon from './NavbarIcon'
+import NavLink from './NavLink';
+import Icon from './Icon';
 import '../css/navbar.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = props => (
 	<header>
-		<nav className="navbar_navigation row middle-xs">
-			<div className="navbar_navigation-logo col-xs-6">
-				<div className="navbar_logo"><Link to="/">Brian Renke</Link></div>
-				<NavbarIcon />
+		<nav className="navbar row middle-xs">
+			<div className="logo col-xs-6">
+				<div><Link to="/">Brian Renke</Link></div>
+				<Icon />
 			</div>
-			<div className="navbar_navigation-links col-xs-6">
+			<div className="links col-xs-6">
 				<MenuButton click={props.sideDrawerClick} />
 				<ul>
-					<Link to="/about">
-						<li>About</li>
-					</Link>
-					<Link to="/work">
-						<li>Work</li>
-					</Link>
-					<Link to="/contact">
-						<li>Contact</li>
-					</Link>
+					<NavLink link="/about">About</NavLink>
+					<NavLink link="/work">Work</NavLink>
+					<NavLink link="/contact">Contact</NavLink>
 				</ul>
 			</div>
 		</nav>
